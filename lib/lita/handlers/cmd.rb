4149c866f4ac4bf23f26129401a61d9966f2930d
Devon Finninger <devon.finninger@peopleadmin.com>
2015-05-27 11:53:40 -0500

    works

--- lib/lita/handlers/cmd.rb
+++ lib/lita/handlers/cmd.rb
@@ -1,6 +1,41 @@
+require 'open3'
+
 module Lita
   module Handlers
     class Cmd < Handler
+
+      config :scripts_dir
+
+      route(/^\s*cmd-help\s*$/, command: true, help: {
+        "cmd-help" => "get a list of scripts available for execution"
+      }) do |resp|
+        Dir.chdir(config.scripts_dir)
+        Dir.glob('*').each { |d| resp.reply d }
+      end
+
+      route(/^\s*cmd\s+(\S*)\s*(.*)$/, :cmd, command: true, help: {
+        "cmd SCRIPT" => "run the SCRIPT specified; use `lita cmd help` for a list"
+      })
+
+      def cmd(resp)
+        dir = config.scripts_dir
+        script = resp.matches[0][0]
+        opts = resp.matches[0][1]
+        Dir.chdir('/tmp')
+
+        out = String.new
+        err = String.new
+        Open3.popen3("#{dir}/#{script}", opts) do |i, o, e, wait_thread|
+          o.each { |line| out << "[ #{script} :: stdout ] #{line}" }
+          e.each { |line| err << "[ #{script} :: stderr ] #{line}" }
+        end
+
+        if err != String.new
+          out << "\n\n#{err}"
+        end
+        resp.reply out
+      end
+
     end
 
     Lita.register_handler(Cmd)
